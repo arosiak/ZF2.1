124d7e7798001e9169f0c83269927efe42c52857
Artur <a.rosiak@hot.pl>
2015-03-05 11:47:57 +0100

    Story 101: Fix benchmarks for speed

--- public/test5.php
+++ public/test5.php
@@ -1,15 +1,16 @@
 <?
-		$x = microtime();
-		for($i=1; $i<100000000; $i++) {
-			$temp = rand(1,1000000);
-		}
-		$y = microtime();
-		
-		$x1 = explode(' ', $x);
-		$x = (real)$x1[1] + (real)$x1[0];
+	// Test bez poczekalni
+	$x = microtime();
+	for($i=1; $i<100000000; $i++) {
+		$temp = rand(1,1000000);
+	}
+	$y = microtime();
+	
+	$x1 = explode(' ', $x);
+	$x = (real)$x1[1] + (real)$x1[0];
 
-		$y1 = explode(' ', $y);
-		$y = (real)$y1[1] + (real)$y1[0];
-		
-		echo $y - $x;
+	$y1 = explode(' ', $y);
+	$y = (real)$y1[1] + (real)$y1[0];
+	
+	echo $y - $x;
 ?>
